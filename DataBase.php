8b29067010f0c738716332ab29db41cd66f18b5b
Abir Hasan <67853959+XAbirHasan@users.noreply.github.com>
2021-03-14 13:33:05 +0600

    Initial commit

---
+++ DataBase.php
@@ -0,0 +1,81 @@
+<?php
+require "DataBaseConfig.php";
+
+class DataBase
+{
+    public $connect;
+    public $data;
+    private $sql;
+    protected $servername;
+    protected $username;
+    protected $password;
+    protected $databasename;
+
+    public function __construct()
+    {
+        $this->connect = null;
+        $this->data = null;
+        $this->sql = null;
+        $dbc = new DataBaseConfig();
+        $this->servername = $dbc->servername;
+        $this->username = $dbc->username;
+        $this->password = $dbc->password;
+        $this->databasename = $dbc->databasename;
+    }
+
+    function dbConnect()
+    {
+        $this->connect = mysqli_connect($this->servername, $this->username, $this->password, $this->databasename);
+        return $this->connect;
+    }
+
+    function prepareData($data)
+    {
+        return mysqli_real_escape_string($this->connect, stripslashes(htmlspecialchars($data)));
+    }
+
+    function logIn($table, $email, $password)
+    {
+        $email = $this->prepareData($email);
+        $password = $this->prepareData($password);
+
+        $this->sql = "select * from " . $table . " where email = '" . $email . "'";
+        
+        $result = mysqli_query($this->connect, $this->sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) != 0) {
+            $dbemail = $row['email'];
+            $dbpassword = $row['password'];
+            if ($dbemail == $email && password_verify($password, $dbpassword)) {
+                $login = true;
+            } else $login = false;
+        } else $login = false;
+
+        return $login;
+    }
+
+    function signUp($table, $name, $email, $phone, $password)
+    {
+        $name = $this->prepareData($name);
+        $phone = $this->prepareData($phone);
+        $password = $this->prepareData($password);
+        $email = $this->prepareData($email);
+
+        // this is for bcrypt 
+        // salt round 10
+        $options = [
+            'cost' => 10,
+        ];
+
+        $password = password_hash($password, PASSWORD_BCRYPT, $options);
+
+        $this->sql =
+            "INSERT INTO " . $table . " (name, phone, password, email) VALUES ('" . $name . "','" . $phone . "','" . $password . "','" . $email . "')";
+        if (mysqli_query($this->connect, $this->sql)) {
+            return true;
+        } else return false;
+    }
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,54 @@
+![](https://komarev.com/ghpvc/?username=XAbirHasan&color=brightgreen)
+
+All credit goes to [@VishnuSivadasVS](https://github.com/VishnuSivadasVS)  check out his work.
+
+# LogIn and SignUp API using php with encryption.
+A secure PHP API to manage login and signup operations. This API protects from attacks like SQL injunction and XSS. Also have password encryption. You can chnage anyting you want.
+
+## How to use the API?
+First of all download the files DataBase.php and DataBaseConfig.php. Place it in your project folder. Go to the DataBaseConfig.php file and change the details to your database details. Now add the file DataBase.php to your project file by requiring it.
+```
+require "DataBase.php";
+```
+Simply create an object for the class DataBase.
+```
+$db = new DataBase();
+```
+Call the dbConnect() and it will return a boolean value which shows whether the database connection is success of not. 
+```
+$db->dbConnect();
+```
+Inside the current file check the condition for database connection. If it is true proceed with the rest. Check this sample code.
+```
+require "DataBase.php";
+$db = new DataBase();
+if ($db->dbConnect()) {
+    //Database connection is success.
+}
+else{
+    //Database connection is failed.
+}
+```
+Now that the connection is success you can call the login and the signup functions. Use the object of the class DataBase and call the functions.
+
+### Calling Login Function
+Call the logIn() with the object and pass the tablename, email and password as its arguments. Changes required as your database and need.
+```
+$db->logIn($tablename, $email, $password);
+```
+
+### Calling Signup Function
+Call the signUp() with the object and pass tablename, name, phone, password and email as its arguments. hanges required as your database and need.
+```
+$db->signUp($tablename, $name, $phone, $password, $email);
+```
+
+Feel free to modify in this code.
+
+## Authors
+
+* **XAbirHasan**
+
+Check out my other works [@XAbirHasan](https://github.com/XAbirHasan)
+
+All credit goes to [@VishnuSivadasVS](https://github.com/VishnuSivadasVS)  check out his work.
